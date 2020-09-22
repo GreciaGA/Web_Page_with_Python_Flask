c125dfe0a2cb18273981dfd76652d2f8a69f30ea
GreciaGA <grecia.anicama@gmail.com>
2020-09-22 16:32:30 -0500

    Subiendo mi web con python y flask

---
+++ src/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # return 'Hello Home Grecia'
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    # debug es para decir que estoy en ambiente de prueba y necesito que actualice constantmnt
+    app.run(debug=True)
